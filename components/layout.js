eff1511986887075a5646f65a57ddf983a50b250
stefanatanasovmkd <atanasovstefan@hotmail.com>
2022-08-24 16:20:51 +0300

    trying to change photo

--- components/layout.js
+++ components/layout.js
@@ -17,12 +17,7 @@ export default function Layout({ children, home }) {
             name="description"
             content="Stefan Atanasov personal blog page"
           />
-          <meta
-            property="og:image"
-            content={`https://og-image.vercel.app/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
+          <meta property="og:image" content="images/profile.png" />
           <meta name="og:title" content={siteTitle} />
           <meta name="twitter:card" content="summary_large_image" />
           <link
